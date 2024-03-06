a3ac306bc7906128697d6c1db9c4b5aace91f89f
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2024-03-06 02:18:19 -0500

    [ADD] Created user profile component with basic routing

---
+++ client/src/Profile.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Profile = ({currentUser}) => {
+    const navigate = useNavigate();
+
+    const [userAssociatedDogs, setUserAssociatedDogs] = useState([]);
+
+    useEffect(() => {
+		fetch(`/api/users/${currentUser.id}/dogs`)
+		.then((response) => response.json())
+        .then((data) => setUserAssociatedDogs([...data]));
+	}, []);
+
+    return (
+        <div className={"mainContainer"}>
+            <div className={"titleContainer"}>
+                Profile: <br /><small><i>{currentUser.username}</i></small>
+            </div>
+            <br />
+            <h3>Your Dogs:</h3>
+                {
+                    userAssociatedDogs.map((adoptedDog) => <div><button>{adoptedDog.name} ({adoptedDog.breed})</button></div>)
+                }
+            <div className={"buttonContainer"}>
+                <input className={"inputButton"} type="button" onClick={() => navigate("/")} value="HOME" />
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
